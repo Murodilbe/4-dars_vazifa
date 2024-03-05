cb7b9aa4203618cb852ac4d99d9e2651719b9e9f
Murodilbe <murodilxolmatov73@gmail.com>
2024-03-05 12:10:01 +0500

    barcha fayllar qushildi

---
+++ vazifa 4-dars.py
@@ -0,0 +1,12 @@
+############
+#  1-MASALA
+# from collections import namedtuple
+# BOOKS = namedtuple("BOOKS",'id, name, price, made_year')
+# books = [
+#     (1, "shaytanat", 1000, 1993),
+#     (2, "molxona", 1200, 2515)]
+# for book in books:
+#     bk = BOOKS(*book)
+#     print(bk.id, bk.name, bk.price, bk.made_year)
+
+
